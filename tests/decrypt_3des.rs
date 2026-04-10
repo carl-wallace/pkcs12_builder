6d0f42a148bbb4a533f5477464b8144693886f5f
Carl Wallace <carl@redhoundsoftware.com>
2026-04-10 06:31:30 -0400

    use pyca example and check all against an expected key hash

--- tests/decrypt_3des.rs
+++ tests/decrypt_3des.rs
@@ -2,6 +2,7 @@
 #![cfg(feature = "legacy")]
 
 use pkcs12_builder::get_key_and_cert;
+use sha2::{Digest, Sha256};
 
 #[test]
 fn decrypt_3des() {
@@ -12,6 +13,10 @@ fn decrypt_3des() {
     let contents1 = get_key_and_cert(p12_iter1, password).unwrap();
     let contents2048 = get_key_and_cert(p12_iter2048, password).unwrap();
     let contents100000 = get_key_and_cert(p12_iter100000, password).unwrap();
+    let rsa_key_der_sha256 =
+        hex_literal::hex!("ccdf40f8d0881c5aa3cb9c563399f5fb590f7615ef7da4d057031bc809c9190d");
+    let rsa_digest = Sha256::digest(contents1.key_der.clone());
+    assert_eq!(rsa_digest.as_slice(), rsa_key_der_sha256);
     assert_eq!(contents1.key_id, contents2048.key_id);
     assert_eq!(contents1.key_der, contents2048.key_der);
     assert_eq!(contents1.certificate, contents2048.certificate);
@@ -28,4 +33,11 @@ fn decrypt_3des() {
         contents1.additional_certificates,
         contents100000.additional_certificates
     );
+
+    let p12_pyca = include_bytes!("data/pyca-cert-rc2-key-3des.p12");
+    let contents_pyca = get_key_and_cert(p12_pyca, "cryptography").unwrap();
+    let pyca_ec_key_der_sha256 =
+        hex_literal::hex!("956890dd43249260db8b4a7edf87541070086c186f6a5e39e2eba2eec28f634c");
+    let ec_digest = Sha256::digest(contents_pyca.key_der.clone());
+    assert_eq!(ec_digest.as_slice(), pyca_ec_key_der_sha256);
 }
